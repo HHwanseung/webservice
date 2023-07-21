a92c528e812ecd5cb7057666ac53249a9d6e020e
seunghwanLee <dkqlch@naver.com>
2023-07-21 21:24:51 +0900

    SecurityConfig - antMatchers 정리

--- src/main/java/com/hwan/webservice/config/auth/SecurityConfig.java
+++ src/main/java/com/hwan/webservice/config/auth/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.hwan.webservice.config.auth;
 import com.hwan.webservice.domain.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,7 +20,7 @@ public class SecurityConfig {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
