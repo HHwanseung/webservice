83323cedc26ff9409e05676bbff21c790dd191b5
seunghwanLee <dkqlch@naver.com>
2023-07-27 19:12:22 +0900

    ec2를 위한 application.yml 분리

--- src/main/java/com/hwan/webservice/WebserviceApplication.java
+++ src/main/java/com/hwan/webservice/WebserviceApplication.java
@@ -2,9 +2,11 @@ package com.hwan.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@PropertySource("classpath:/aws.properties")
 public class WebserviceApplication {
 
 	public static void main(String[] args) {
